177b7d8b5ca111baeaae5271f3e892b6439d9501
KOMOTO Keita <125565087+keita-komoto@users.noreply.github.com>
2024-06-05 16:20:00 +0900

    faet: base page setting

--- next/src/pages/_document.tsx
+++ next/src/pages/_document.tsx
@@ -2,12 +2,17 @@ import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html lang="en">
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
+    <Html lang="jp">
+    <Head>
+      <meta charSet="utf-8" />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <meta name="keyword" content="スノーボード,ステッカー,印刷,加工,施工,ワンストップオーダー,snowboard,stiker,tune,print,construction" />
+      <meta name="description" content="MAKE UP SNOWBOARD ｜ スノーボードのデッキをオリジナルのデザインでメイクしよう。ワンストップオーダーでデザインから加工まで。デザイナーと直接相談して0からデザインを作ろう ｜ SStune" />
+    </Head>
+    <body className="home">
+      <Main />
+      <NextScript />
+    </body>
+  </Html>
   );
 }
